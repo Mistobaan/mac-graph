17ce0f1867ab38b1655d5c2fd102537097a1f700
David Mack <david@sketchdeck.com>
2018-12-12 20:53:56 -0800

    bulk

--- build-sa.sh
+++ build-sa.sh
@@ -3,5 +3,5 @@
 j=StationAdjacent
 
 nohup python -m macgraph.input.build \
-	--gqa-paths input_data/raw/multistep/gqa-$j* \
+	--gqa-paths input_data/raw/bulk/gqa-$j* \
 	--name $j &> nohup-build-$j.out&
